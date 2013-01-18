11ad3649b401b88a696b24d4a8f7c128c8df0926
Caleb Harris <caleb@webs.com>
2013-01-18 01:33:13 -0500

    Add list of requests

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -9,6 +9,10 @@ class RequestsController < ApplicationController
     redirect_to @request
   end
 
+  def index
+    @requests = Request.all
+  end
+
   def show
     @request = Request.find params[:id]
   end
